41a1717e12fea51611529be2901af10b3071191f
lindabalode <balode.lindaa@gmail.com>
2023-04-27 11:39:37 +0300

    O

--- index.php
+++ index.php
@@ -20,7 +20,12 @@
 <h1>GIFFS</h1>
 
 <button id="showTrending">Show Trending</button>
-
+<form id="trendingForm" action="" method="get" style="display: none">
+    <label for="trendingCount">Enter the number of gifs to display:</label>
+    <input type="text" name="count" id="trendingCount" value="">
+    <button type="submit">Search</button>
+</form>
+<button id="searchButton">Search for gifs</button>
 <form id="searchForm" action="" method="get" style="display: none">
     <label for="keyword">Enter a keyword to search for gifs:</label>
     <input type="text" name="keyword" id="keyword" value="">
@@ -53,10 +58,17 @@ if (isset($_GET['count']) && empty($_GET['keyword'])) {
 
 <script>
     const showTrendingButton = document.getElementById("showTrending");
+    const trendingForm = document.getElementById("trendingForm");
+    const searchButton = document.getElementById("searchButton");
     const searchForm = document.getElementById("searchForm");
 
     showTrendingButton.addEventListener("click", () => {
         showTrendingButton.style.display = "none";
+        trendingForm.style.display = "block";
+    });
+
+    searchButton.addEventListener("click", () => {
+        searchButton.style.display = "none";
         searchForm.style.display = "block";
     });
 </script>
